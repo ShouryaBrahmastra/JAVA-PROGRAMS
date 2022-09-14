f3bbf4c180c297089417ec3768000a92dcc0d9d9
Shourya Gupta <gptshourya02@gmail.com>
2022-09-14 22:19:51 +0530

    second commit

--- 2.5/A/Acc.java
+++ 2.5/A/Acc.java
@@ -1,7 +1,5 @@
 package A;
 
-import B.*;
-
 class Acc {
 	private int id;
 	String name;
@@ -24,16 +22,5 @@ class Acc {
 	public String toString() {
 		return ("ID: " + this.id + ", Name: " + this.name + ", Address: " + this.address + ", PAN Number: "
 				+ this.panNumber + ", Account type: Home Loan");
-	}
-}
-
-public class Main {
-	public static void main(String[] args) {
-		Account acc1 = new Account(128, "Shourya Gupta", "Kolkata", 354548);
-		System.out.println(acc1);
-		acc1.update("Shourya Gupta", "DB block, New Town", 146589);
-		System.out.println(acc1);
-		Account acc2 = new Account(115, "Aditya Azad", "Patna", 834677);
-		System.out.println(acc2);
 	}
 }
